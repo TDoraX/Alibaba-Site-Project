9325958672b03d47bad3da29bf97d5008484304f
TDoraX <zhangze612@163.com>
2019-02-25 15:51:08 +0800

    Initialize Project

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+const { series, src, dest, watch } = require('gulp');
+const sass = require('gulp-sass');
+const html = require('gulp-htmlmin');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const img = require('gulp-imagemin');
+const compass = require('gulp-compass');
+const css = require('gulp-clean-css');
+
+function runSass() {
+    return src('src/sass/*.scss')
+        .pipe(compass({
+            config_file: './config.rb',
+            css: 'src/css',
+            sass: 'src/sass'
+        }))
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(css())
+        .pipe(dest('dist/css/'));
+}
+
+function minifyHTML() {
+    return src('src/*.html')
+        .pipe(html())
+        .pipe(dest('dist/'));
+}
+
+function operateJS() {
+    return src('src/script/js/*.js')
+        .pipe(concat('bundle.js'))
+        .pipe(dest('dist/script/js/'))
+        .pipe(rename('bundle.min.js'))
+        .pipe(uglify())
+        .pipe(dest('dist/script/js/'));
+}
+
+function minifyImg() {
+    return src('src/img/*.png')
+        .pipe(img())
+        .pipe(dest('dist/img/'));
+}
+
+exports.default = series(runSass, minifyHTML, operateJS, minifyImg);
+
+watch('src/*.html', minifyHTML);
+watch('src/sass/*.scss', runSass);
+watch('src/script/js/*.js', operateJS);
