36cb5477eed0e2be43d1809929853038786cffc6
TDoraX <zhangze612@163.com>
2019-02-28 19:12:50 +0800

    Modified stylesheets, structures,  add JS files to the page of index.

--- src/script/js/index.js
+++ src/script/js/index.js
@@ -1,60 +1,104 @@
 define(['config'], function (config) {
     require(['jquery'], function ($) {
-        // 滚动加载
-        function ScrollLoad() {
-            $(window).scroll(function () {
-                var scrollTop = $(this).scrollTop;
-                var scrollHeight = $(document).height();
-                var clientHeight = $(this).height();
-                if (scrollTop + clientHeight >= scrollHeight) {
-
-                }
-            })
+        // 首页模块的构造器
+        function IndexModule() {
+            this._this = this;
         }
 
-        // 左侧菜单栏样式切换
-        function MenuLists() {
-            this.lists = $('.main .left #subnav > li');
-            this.floatLayer = $('.main .left .floatLayer');
-            this.floatText = $('.main .left .floatLayer_text');
-            this.floatImg = $('.main .left .floatLayer_img');
-            this.textMenu = $('.main .left div.text');
-            this.imgMenu = $('.main .left div.image');
-        }
-        MenuLists.prototype.init = function () {
-            var _this = this;
-            this.showLists(_this);
-            this.hideLists(_this);
+        // 首页的初始化方法
+        IndexModule.prototype.init = function () {
+            this.scrollSearch();
+            this.toggleMenu();
+            this.slideShow();
         };
-        MenuLists.prototype.showLists = function (_this) {
-            this.lists.each(function (index) {
-                $(_this.lists[index]).hover(function () {
-                    $(_this.lists[index]).addClass('current').siblings().removeClass('current');
-                    _this.floatLayer.show();
-                    _this.floatText.show();
-                    _this.floatImg.show();
-                    $(_this.textMenu[index]).show().siblings().hide();
-                    $(_this.imgMenu[index]).show().siblings().hide();
-                });
-                _this.floatLayer.hover(function () {
-                    console.log($(this).children().children());
-                    $(this).children().children().show();
-                }, function () {
-                    $(this).hide();
-                });
+
+        // 滚动固定搜索栏功能
+        IndexModule.prototype.scrollSearch = function () {
+            var fixedBar = $('#fixed-search-bar');
+            // 开关变量保证动画只触发一次
+            var derail = false;
+            $(window).on('scroll', function () {
+                if ($(window).scrollTop() >= 200 && !derail) {
+                    derail = true;
+                    fixedBar.animate({
+                        top: 0
+                    });
+                }
+                if ($(window).scrollTop() < 200 && derail) {
+                    derail = false;
+                    fixedBar.animate({
+                        top: '-54px'
+                    });
+                }
             });
         };
-        MenuLists.prototype.hideLists = function (_this) {
-            // _this.floatLayer.on('mouseout', function () {
-            //     if (_this.__switch) {
-            //         _this.lists.each(function (index) {
-            //             $(_this.lists[index]).removeClass('current');
-            //         });
-            //         $(this).hide();
-            //         _this.__switch = false;
-            //     }
-            // })
+
+        // 左侧菜单栏切换功能
+        IndexModule.prototype.toggleMenu = function () {
+            var subNav = $('#subnav');
+            var floatMenu = $('.float-menu');
+            var lists = $('#subnav > li').not(floatMenu);
+            subNav.on('mouseover', function () {
+                floatMenu.stop(true, true).show();
+            });
+            // 需要冒泡，用mouseover事件
+            lists.each(function (index) {
+                $(lists[index]).on('mouseover', function () {
+                    $(this).addClass('current').siblings().removeClass('current');
+                });
+            });
+            // 阻止冒泡，用mouseleave事件
+            subNav.on('mouseleave', function () {
+                lists.each(function (index) {
+                    $(lists[index]).removeClass('current');
+                });
+                floatMenu.stop(true, true).fadeOut(100);
+            });
         };
-        new MenuLists().init();
-    })
+
+        // 幻灯片功能
+        IndexModule.prototype.slideShow = function () {
+            var slides = $('.slide-show ul > li');
+            var prev = $('.slide-show .prev');
+            var next = $('.slide-show .next');
+            var slideButtons = $('.slide-show .slide-btn-list .slide-btn');
+            var playNum = 0;
+            slides.eq(0).animate({opacity: 1}, 200);
+            next.on('click', function () {
+                slides.eq(playNum).animate({opacity: 0}, 200);
+                if (playNum >= slides.length - 1) {
+                    playNum = 0;
+                    slides.eq(playNum).animate({opacity: 1}, 200);
+                } else {
+                    slides.eq(playNum + 1).animate({opacity: 1}, 200);
+                    playNum++;
+                }
+                slideButtons.eq(playNum).addClass('current').siblings().removeClass('current');
+            });
+            prev.on('click', function () {
+                slides.eq(playNum).animate({opacity: 0}, 200);
+                if (playNum <= 0) {
+                    playNum = slides.length - 1;
+                    slides.eq(playNum).animate({opacity: 1}, 200);
+                } else {
+                    slides.eq(playNum - 1).animate({opacity: 1}, 200);
+                    playNum--;
+                }
+                slideButtons.eq(playNum).addClass('current').siblings().removeClass('current');
+            });
+            slideButtons.each(function (index) {
+                slideButtons.eq(index).on('click', function () {
+                    slides.eq(playNum).animate({opacity: 0}, 200);
+                    if (index !== playNum) {
+                        slideButtons.eq(index).addClass('current').siblings().removeClass('current');
+                        slides.eq(index).animate({opacity: 1}, 200);
+                        playNum = index;
+                    }
+                })
+            })
+        };
+
+        // 初始化首页模块
+        new IndexModule().init();
+    });
 });
